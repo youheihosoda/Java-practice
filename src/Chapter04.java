78ea542b000de73e65a5683d2741eaabbd66e6f4
YouheiHosoda <youhe0122@gmail.com>
2021-07-31 23:36:48 +0900

    Java chapter05

---
+++ src/Chapter04.java
@@ -0,0 +1,16 @@
+
+public class Chapter04 {
+
+  public static void main(String[] args) {
+	  
+    String str1;
+    str1 = "tarou";
+    System.out.println(str1);
+    
+    str1 = "gorimutyu";
+    System.out.println(str1);
+    
+    int lucky_number = 7;
+    System.out.println(lucky_number);
+  }
+}
---
+++ src/Chapter05.java
@@ -0,0 +1,24 @@
+
+public class Chapter05 {
+  public static void main(String[] args) {
+    long long1 = 10000;
+    double double1 = 1.234;
+    boolean boolean1 = true;
+    
+    System.out.println(long1);
+    System.out.println(double1);
+    System.out.println(boolean1);
+    
+    Integer i = 100;
+    Float f = 1.23f;
+    Character c = 'う';
+    
+    System.out.println(i);
+    System.out.println(f);
+    System.out.println(c);
+  
+    int[] array5 = {1,2,3,4,5};
+    System.out.println(array5[3]);
+  }
+	
+}
