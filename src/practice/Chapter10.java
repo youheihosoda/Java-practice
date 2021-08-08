b615b77846a347a9868b972ee52209fe131919ac
YouheiHosoda <youhe0122@gmail.com>
2021-08-08 16:45:07 +0900

    8/8

--- src/practice/Chapter10.java
+++ src/practice/Chapter10.java
@@ -3,6 +3,8 @@ package practice;
 import java.sql.SQLException;
 
 public class Chapter10 {
+	public static void main(String[] args) {
+	
 	
 	try {
 		int array[] = {1,3,5};
@@ -10,9 +12,30 @@ public class Chapter10 {
 		Chapter10.validIndex(array,3);
 		
 		System.out.println("validIndexの呼び出し終了");
-	}catch(IllegalArgumentException e) {
+	} catch(IllegalArgumentException e) { 
 		System.out.println("IllegalArgumentExeptinが発生しました");
 		e.printStackTrace();
 	}
-
+	
+	try {
+		Chapter10.throwSOLException();
+	} catch(SQLException e) {
+		System.out.println("SQLExceptionが発生しました");
+		
+		e.printStackTrace();
+	} finally {
+		System.out.println("throwSQLExceptionの呼び出し終了");
+	}
+	
+	System.out.println("mainメソッド終了");
 }
+	public static void validIndex(int[] array, int index) {
+		if(array.length <= index) {
+			throw new IllegalArgumentException(index+ "はサイズの範囲外です");
+		}
+		System.out.println("インデックス" + index + "の要素は" + array[index] + "です");
+	}
+
+	public static void throwSQLException() throws SQLException{
+		throw new SQLExeptin("SQLエラーです");
+	}
